5ab82736530c765cdc2505bc37e4d8e2bed9b4a1
Riddhi Suteri <ridsuteri@gmail.com>
2022-08-08 00:25:22 +0530

    testing cra

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header';
+import TinderCards from './component/TinderCards';
+import Footer from './component/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <TinderCards />
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
